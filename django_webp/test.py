8617f8f6cb62db667a8431fcf16156fe6c62b507
Andre Farzat <andrefarzat@gmail.com>
2014-05-04 16:55:08 -0400

    Adding the first test ever

---
+++ django_webp/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class MiddlewareWebpTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def test_one(self):
+        self.assertTrue(True)
