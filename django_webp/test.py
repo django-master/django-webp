82028aec9099e503a3d9d664b423812da426cea3
Andre Farzat <andrefarzat@gmail.com>
2014-05-05 00:52:52 -0400

    Adding the view tests

--- django_webp/test.py
+++ django_webp/test.py
@@ -3,8 +3,24 @@ import unittest
 
 from django.template import Template, Context
 from django.core.urlresolvers import reverse
+from django.test.client import Client
+from django.contrib.staticfiles import finders
 
 from django_webp.templatetags.webp import webp
+
+
+USER_AGENTS = [
+    # Chrome
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10',
+
+    # Opera
+    'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14',
+    'Mozilla/5.0 (Windows NT 6.0; rv:2.0) Gecko/20100101 Firefox/4.0 Opera 12.14',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52',
+]
+
 
 
 class TemplateTagTest(unittest.TestCase):
@@ -22,6 +38,38 @@ class TemplateTagTest(unittest.TestCase):
     def test_templatetag_in_template(self):
         rendered = self.render_template('{% load webp %}{% webp "path/to/the/image.png" %}')
         self.assertEqual(self.expected, rendered)
+
+
+class ViewIndexTest(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('django_webp', args=('django_webp/python.png', ))
+        self.image_png_path = finders.find('django_webp/python.png')
+        self.image_webp_path = finders.find('django_webp/python.webp')
+
+
+    def test_simple_request(self):
+        """ shoudl return an image converted to webp """
+        image = open(self.image_webp_path).read()
+
+        for user_agent in USER_AGENTS:
+            headers = { 'HTTP_USER_AGENT': user_agent }
+            response = self.client.get(self.url, **headers)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.get('Content-Type'), 'image/webp')
+
+            self.assertTrue(image == response.content, 'Image was not webp')
+
+    def test_404(self):
+        """ Should raise a 404 """
+        url = reverse('django_webp', args=('django_webp/not-exist.png', ))
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
 
 
 class MiddlewareWebpTest(unittest.TestCase):
--- django_webp/views.py
+++ django_webp/views.py
@@ -1,5 +1,55 @@
 # -*- coding: utf-8 -*-
- 
+import httpagentparser
+import imghdr
+
+from PIL import Image
+from StringIO import StringIO
+
+from django.http import HttpResponse, Http404
+from django.contrib.staticfiles import finders
+
+
+VALID_BROWSERS = ['Chrome', 'Opera', 'Opera Mobile']
+
+def _return_static_image(image_path, content_type=None):
+    if not content_type:
+        content_type = "image/%s" % imghdr.what(image_path)
+
+    image = open(image_path)
+    return HttpResponse(image.read(), content_type=content_type)
+
+
+def _return_webp_image(image_path):
+    data = StringIO()
+    image = Image.open(image_path)
+    try:
+        image.save(data, 'WEBP')
+    except KeyError:
+        return _return_static_image(image_path)
+
+    return HttpResponse(data.getvalue(), content_type="image/webp")
+
+
+def _is_valid_browser(user_agent):
+    if user_agent:
+        data = httpagentparser.detect(user_agent)
+        if 'browser' in data:
+            return data['browser']['name'] in VALID_BROWSERS
+
+    return False
+
 
 def index(request, image_path=""):
-    pass
+    image = finders.find(image_path)
+    if not image:
+        raise Http404()
+
+    user_agent = request.META.get('HTTP_USER_AGENT')
+    if not _is_valid_browser(user_agent):
+        return _return_static_image(image_path)
+
+    return _return_webp_image(image)
+
+
+
+    
