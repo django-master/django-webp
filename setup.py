f432724126127a6b7e1fb2798bc912605ef7b1d1
Andre Farzat <andrefarzat@gmail.com>
2016-11-01 08:38:22 -0200

    Changing the description

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import find_packages
 
 setup(
     name='django-webp',
-    version='1.0.2',
+    version='1.0.3',
     author=u'Andre Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/django-webp/',
     license='LICENSE',
-    description='django webp',
+    description='Returns a webp image instead of jpg, gif or png to browsers which have support',
     long_description=open('README.md').read(),
     install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
