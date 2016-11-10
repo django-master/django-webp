b4a6e77bc7ae031e0b0e9ca12496d15b0b9b3aad
Andre Farzat <andrefarzat@gmail.com>
2016-11-10 12:06:20 -0500

    tag 1.0.6

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages
 
 setup(
     name='django-webp',
-    version='1.0.5',
+    version='1.0.6',
     author=u'Andre Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/django-webp/',
     license='MIT',
     description='Returns a webp image instead of jpg, gif or png to browsers which have support',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
     zip_safe=False,
