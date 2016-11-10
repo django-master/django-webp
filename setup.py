3d7f33f006a5dc8e36a7473cc101b6f590f507c5
Andre Farzat <andrefarzat@gmail.com>
2016-11-10 11:59:36 -0500

    tag 1.0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-webp',
-    version='1.0.4',
+    version='1.0.5',
     author=u'Andre Farzat',
     author_email='andrefarzat@gmail.com',
     packages=find_packages(),
