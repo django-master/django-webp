ec139812c89df3c2f29f5527d16b56a3129d1aa6
Andre Farzat <andre@ed.company>
2020-05-17 19:39:42 -0300

    Fixing test settings to implement the runserver local

--- testapp/settings.py
+++ testapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p9tssl6+ve_5s6(d7^p=^i+_!x()547_v=3qyeg388zv+2rqxb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_webp.context_processors.webp',
             ],
         },
     },
@@ -123,3 +124,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
+
+WEBP_DEBUG = False
--- testapp/urls.py
+++ testapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from testapp.views import index
 
-urlpatterns = [
-    path('', index),
-] + staticfiles_urlpatterns()
 
 # Only for test purposes
+
+urlpatterns = [
+    path('', index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
