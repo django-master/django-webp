0e39a0f332e1530a8c576086ca0fbe4e33b19d6b
Andre Farzat <andre@ed.company>
2020-05-26 19:07:43 -0300

    Atualizando setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-from distutils.core import setup
-from setuptools import find_packages
+import setuptools
+
 
 with open('README.rst') as file:
     long_description = file.read()
 
-setup(
+
+setuptools.setup(
     name='django-webp',
     version='2.0.0',
     author=u'Andre Farzat',
     author_email='andrefarzat@gmail.com',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     url='http://pypi.python.org/pypi/django-webp/',
     license='MIT',
     description='Returns a webp image instead of jpg, gif or png to browsers which have support',
@@ -18,4 +18,5 @@ setup(
     install_requires=open('requirements.txt').readlines(),
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=3.5',
 )
