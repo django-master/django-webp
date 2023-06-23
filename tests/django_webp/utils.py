b4f745a638d3487fe3937c9bb4aa42c8189285a7
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2023-06-22 22:52:23 -0400

    refactored codebase, added support for image links, and accounted for varying production level static file setups in implementation

--- tests/django_webp/utils.py
+++ tests/django_webp/utils.py
@@ -4,20 +4,20 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.templatetags.static import static
 
+if "whitenoise.middleware.WhiteNoiseMiddleware" in settings.MIDDLEWARE:
+    USING_WHITENOISE = True
+else:
+    USING_WHITENOISE = False
 
-STATIC_ROOT = getattr(settings, 'STATIC_ROOT', '') or ''
+STATIC_ROOT = getattr(settings, "STATIC_ROOT", "") or ""
 
-WEBP_STATIC_ROOT = getattr(settings, 'WEBP_STATIC_ROOT', 'WEBP_CACHE')
+WEBP_CHECK_URLS = getattr(settings, "WEBP_CHECK_URLS", "") or False
+
+WEBP_STATIC_ROOT = getattr(settings, "WEBP_STATIC_ROOT", "WEBP_CACHE")
 WEBP_STATIC_ROOT = os.path.join(STATIC_ROOT, WEBP_STATIC_ROOT)
+WEBP_STATIC_URL = static(getattr(settings, "WEBP_STATIC_URL", "WEBP_CACHE/"))
 
-WEBP_STATIC_URL = static(getattr(settings, 'WEBP_STATIC_URL', 'WEBP_CACHE/'))
+WEBP_DEBUG = getattr(settings, "WEBP_DEBUG", settings.DEBUG)
 
-
-WEBP_DEBUG = getattr(settings, 'WEBP_DEBUG', settings.DEBUG)
-
-
-WEBP_CONVERT_MEDIA_FILES = getattr(settings, 'WEBP_CONVERT_MEDIA_FILES', False)
-
-
-if not WEBP_STATIC_URL.endswith('/'):
+if not WEBP_STATIC_URL.endswith("/"):
     raise ImproperlyConfigured("If set, WEBP_STATIC_URL must end with a slash")
